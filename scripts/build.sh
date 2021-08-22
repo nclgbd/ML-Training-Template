f31060e98056a6232ec950770a2d78ca5c610312
Nicole Gu <nicoleguob@gmail.com>
2021-08-22 01:37:05 -0700

    Maybe it works now...?

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,10 @@ echo "$CONDA/bin" >> "$GITHUB_PATH"
 conda activate torch_base
 
 python build.py
-dvc get https://github.com/nclgbd/ML-Data-Configurations.git data/data.zip -o data/
-dvc pull
+
+dvc get https://github.com/nclgbd/ML-Data-Configurations.git test_data/test_data.zip -o --verbose --show-url
+dvc pull --verbose
+unzip test_data.zip -d test_data/
+rm -rf test_data.zip
+
 unzip data/data.zip
--- scripts/run-all-tests.sh
+++ scripts/run-all-tests.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 ls -la
-
-dvc get https://github.com/nclgbd/ML-Data-Configurations.git test_data/test_data.zip -o 
-dvc pull
-unzip test_data.zip -d test_data/
-rm -rf test_data.zip
-
 pytest -v -s
