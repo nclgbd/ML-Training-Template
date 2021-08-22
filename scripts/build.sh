454918e3e37225b25c79a2905b2522ac048cbf7e
Nicole Gu <nicoleguob@gmail.com>
2021-08-22 01:07:34 -0700

    Forgot to activate the env

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "$CONDA/bin" >> "$GITHUB_PATH"
+conda activate torch_base
+
 python build.py
 dvc get https://github.com/nclgbd/ML-Data-Configurations.git data/data.zip -o data/
 dvc pull
